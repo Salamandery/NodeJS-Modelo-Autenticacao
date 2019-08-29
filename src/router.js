92bd11c6df0ff326ddd2825c1e60ab147f797b8b
salamandery <sp.ront@hotmail.com>
2019-08-29 19:10:43 -0300

    .editorconfig

--- src/router.js
+++ src/router.js
@@ -1,15 +1,16 @@
-const { Router } = require('express');
+const { Router } = require("express");
 
 class Routes {
-    constructor(){
-        this.Routers = new Router();
-        this.routes();
-    }
-    routes() {
-        this.Routers.get('/', (req, res)=> {
-            return res.json({message: "consegui"});
-        });
-    }
+  constructor() {
+    this.Routers = new Router();
+    this.routes();
+  }
+
+  routes() {
+    this.Routers.get("/", (req, res) => {
+      return res.json({ message: "consegui" });
+    });
+  }
 }
 
 module.exports = new Routes().Routers;
