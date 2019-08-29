6ae1505232f11ab97d49738934de93ad6ba2a44d
salamandery <sp.ront@hotmail.com>
2019-08-29 07:40:22 -0300

    Commiting all files

---
+++ src/router.js
@@ -0,0 +1,15 @@
+const { Router } = require('express');
+
+class Routes {
+    constructor(){
+        this.Routers = new Router();
+        this.routes();
+    }
+    routes() {
+        this.Routers.get('/', (req, res)=> {
+            return res.json({message: "consegui"});
+        });
+    }
+}
+
+module.exports = new Routes().Routers;
